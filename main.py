31998b08d6463b35bf4772facb40d2c801f2d7de
Angel Bustamante <angelbustamantezuniga@gmail.com>
2024-06-17 19:19:17 -0400

    Fix
    Se modifico bien el menu y se arreglo el sueldo de los trabajadores con sus puestos

--- main.py
+++ main.py
@@ -11,13 +11,13 @@ if user_login == nombre and user_passw == passw:
         print("\n¿Que accion desea realizar?\n1.-Registrar Trabajador\n2.-Listar los todos los trabajadores\n3.-Imprimir planilla de sueldos\n4. Salir del Programa")
         opc=int(input("Elija una opcion:"))
         if opc == 1:
-            
+            print("Resgistrar trabajador")
         elif opc == 2:
-
+            print("Listado de todos los trabajadores")
         elif opc == 3:
-
+            print("Plantilla de sueldos")
         elif opc == 4:
+            print("Saliendo del programa...")
             break
-
 else:
     print("usuario o/y contraseña incorrectos...\nVuelva interntarlo")
