1d4a2401c8417561fc81eb419ae12234857f6783
Angel Bustamante <angelbustamantezuniga@gmail.com>
2024-06-17 16:40:20 -0400

    Se agrego el menu inicial
    solo es el menú y no tiene ninguna opción

---
+++ main.py
@@ -0,0 +1,15 @@
+
+flag=True
+#Login
+#Menu
+while flag == True:
+    print("\n¿Que accion desea realizar?\n1.-Registrar Trabajador\n2.-Listar los todos los trabajadores\n3.-Imprimir planilla de sueldos\n4. Salir del Programa")
+    opc=int(input("Elija una opcion:"))
+    if opc == 1:
+        
+    elif opc == 2:
+
+    elif opc == 3:
+
+    elif opc == 4:
+        break
