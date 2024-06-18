c7d61a147e13b1858492e5f83dd49489bf20c1ca
Angel Bustamante <angelbustamantezuniga@gmail.com>
2024-06-17 21:50:52 -0400

    Actualización 0.1 EPICA [ACTUALIZACION]
    -Se agrego La opción 1
    -Se cambio el formato del archivo trabajadores.csv

--- funciones.py
+++ funciones.py
@@ -1,4 +1,14 @@
 import csv
+#Registrar Trabajador
+def RegistrarTrabajador():
+    nombre=input("ingrese nombre y apellido del trabajador: ")
+    cargo=input("ingrese cargo del trabajador: ")
+    sueldo=int(input("ingrese Sueldo Bruto del trabajador: "))
+    with open('trabajadores.csv', 'a', newline='\n') as archivo_csv:
+        escritor_csv = csv.writer(archivo_csv)
+        escritor_csv.writerow([nombre,cargo,sueldo])
+    print("\nTrabajador registrado exitosamente :)")
+#listado de trabajadores
 def ListadoTrabajadores():
     with open('trabajadores.csv', mode='r', newline='',encoding='Utf-8') as archivo_csv:
         lector_csv = csv.DictReader(archivo_csv)
--- main.py
+++ main.py
@@ -5,15 +5,16 @@ flag=True
 nombre="guest"
 passw="1234"
 print("Inicio de secion\n")
-user_login=input("Ingrese usuario: ")
+user_login=input("Ingrese usuario: ".lower())
 user_passw=input("Ingrese contraseña: ")
 if user_login == nombre and user_passw == passw:
 #Menu
     while flag == True:
-        print("\n¿Que accion desea realizar?\n1.-Registrar Trabajador\n2.-Listar los todos los trabajadores\n3.-Imprimir planilla de sueldos\n4. Salir del Programa")
+        print("\n¿Que accion desea realizar?\n1.-Registrar Trabajador\n2.-Listar los todos los trabajadores\n3.-Imprimir planilla de sueldos\n4.-Salir del Programa")
         opc=int(input("Elija una opcion:"))
         if opc == 1:
             print("Resgistrar trabajador")
+            fun.RegistrarTrabajador()
         elif opc == 2:
             fun.ListadoTrabajadores()
         elif opc == 3:
